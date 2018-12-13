ce0d5de1897b1de3c9f3b4bc7e698d3437244fe8
thangylvp <thang.yenlacvinhphuc.98@gmail.com>
2018-12-13 15:51:37 +0700

    Add client send list file to server

--- Server/src/server.c
+++ Server/src/server.c
@@ -22,30 +22,54 @@ void sendAccept(int *fd) {
     int message = 1;
     write(*fd, &message, sizeof(message));
 }
-void * sendFile(void * file_description) {
-    int fd = *((int *) file_description);
-    int n_bytes;
-    int message;
 
-    // message = 1;
-    // write(fd, &message, sizeof(message));
+void recivelistFile(int *fd) {
+    int n_bytes;
+    int nFile, fileSize, fileLen;
+    char fileName[32];
+    n_bytes = read(*fd, &nFile, sizeof(int));
+    printf("Num of file : %d\n", nFile);
+    
+    for (int i = 0; i < nFile; i++) {
+        n_bytes = read(*fd, &fileName, 32);
+        printf("name %d %d %s\n", i, n_bytes, fileName);
+        n_bytes = read(*fd, &fileSize, sizeof(int));
+        printf("len %d %d\n", n_bytes, fileSize);
+        
+        //n_bytes = read(fd, fileName, fileLen);
+        //fileName[n_bytes-1] = '\0';
+
+        
+        // printf("infor file : %s %d\n", fileName, fileSize);
+    }
+    
+}
+
+void * process(void * file_description) {
+    int fd = *((int *) file_description);
+    int n_bytes, message;
 
     sendAccept(&fd);   
+    
     LOOP:
-
-    
-
+  
     n_bytes = read(fd, &message, sizeof(int));
-    
+    printf("new message %d\n", message);
     if (message == -1) {
         printf("* Disconnect client port: %d.\n", listPort[fd]);
         return NULL;
-    }  else {
+    }  else 
+    if (message == 2) {
+        printf("client want to send list file\n");
+        sendAccept(&fd);
+        recivelistFile(&fd);
+    } else 
+    if (message == 3){
+        printf("Message : %d\n", message);
+    } else {
         printf("Message : %d\n", message);
     }
 
-    
-    
 
     goto LOOP;
     
@@ -71,7 +95,7 @@ int main(int argc, char **argv) {
         clisize= sizeof(client);
         *iptr = accept(serverfd, (struct sockaddr*) &client, &clisize);
         printf("\nREQUEST FROM %s PORT %d \n",inet_ntop(AF_INET,&client.sin_addr,str,sizeof(str)),htons(client.sin_port));
-        pthread_create(&tid, NULL, &sendFile, (void *) iptr);
+        pthread_create(&tid, NULL, &process, (void *) iptr);
         listPort[*iptr] = htons(client.sin_port);
         
 
